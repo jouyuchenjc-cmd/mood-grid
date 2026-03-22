7f8c45cdd9d1dfb2bc3cd0cff7b586913e4fbff8
jouyuchenjc-cmd <jouyu.chen.jc@gmail.com>
2026-03-22 19:46:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
-const CACHE_NAME = 'moodgrid-v2';
+const CACHE_NAME = 'moodgrid-v3';
 const ASSETS = [
   './',
   './index.html',
   'https://fonts.googleapis.com/css2?family=Sulphur+Point:wght@300;400;700&family=Noto+Sans+TC:wght@300;400;500&family=Noto+Sans+JP:wght@300;400;500&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js',
+  './badges/Collection-01.svg',
+  './badges/Collection-02.svg',
+  './badges/Collection-03.svg',
+  './badges/Collection-04.svg',
+  './badges/Collection-05.svg',
+  './badges/Collection-06.svg',
+  './badges/Collection-07.svg',
+  './badges/Collection-08.svg',
+  './badges/Collection-09.svg',
+  './badges/Collection-10.svg',
+  './badges/Collection-11.svg',
+  './badges/Collection-12.svg',
+  './badges/Collection-13.svg',
+  './badges/Collection-14.svg',
+  './badges/Collection-15.svg',
+  './badges/Collection-16.svg',
+  './badges/Collection-17.svg',
 ];
 
 self.addEventListener('install', e => {
